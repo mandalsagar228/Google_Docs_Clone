29b00ead7d339645e94b2b9ba3ffe3ab0ef13794
Sagar Mandal <69377939+mandalsagar228@users.noreply.github.com>
2023-09-10 07:45:42 +0545

    frontend url added

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 9000;
 
 const io = new Server(PORT, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://googledocsclonee.netlify.app",
     methods: ["GET", "POST"],
   },
 });
